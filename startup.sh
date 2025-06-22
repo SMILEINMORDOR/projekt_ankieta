ab85035f5dd2bb472c44baaf007bbf0c669cc90c
SMILEINMORDOR <273748@student.pwr.edu.pl>
2025-06-22 23:46:27 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y unixodbc-dev
-gunicorn --bind=0.0.0.0:8000 app:app
+apt-get update && apt-get install -y unixodbc-dev g++
+pip install --upgrade pip
+pip install -r requirements.txt
+
+gunicorn --bind=0.0.0.0:8000 --timeout 600 app:app
